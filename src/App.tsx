2f38db853e129c97a378c3cb1f7a9ad05835307a
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-30 18:46:38 +0000

    Updated app with components and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Home, Store } from './components';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex h-full flex-col bg-base-300">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
