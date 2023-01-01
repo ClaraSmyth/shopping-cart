3169cd8a7f01875cdd42b444de929a6a1592f71c
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-01 11:49:13 +0000

    Updated Store layout

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 function Store() {
   return (
-    <div className="grid auto-rows-max grid-cols-4 gap-4 overflow-scroll p-4">
+    <div className="grid auto-rows-max grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-4 overflow-scroll p-4">
       <Card></Card>
       <Card></Card>
       <Card></Card>
