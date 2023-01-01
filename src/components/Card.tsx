259183d4a6da7841bce87d9c577485e86d65b6bf
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-01 22:49:03 +0000

    Layout changes to make Cards more responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,15 @@ import { MdOutlineAddShoppingCart } from 'react-icons/md';
 
 function Card() {
   return (
-    <div className="card-compact card bg-base-100 shadow-xl max-[430px]:card-side">
-      <figure>
+    <div className="card-compact border-rad card overflow-hidden bg-base-100 shadow-xl max-[430px]:grid max-[430px]:grid-cols-[40%,1fr]">
+      <figure className="overflow-hidden rounded-none">
         {/* Card Image */}
-        <img className="h-full" src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img className="h-full object-cover" src="https://placeimg.com/400/225/arch" alt="Shoes" />
       </figure>
       <div className="card-body relative">
         {/* Title */}
-        <h2 className="custom-text-ellipsis-2 card-title">Game Name</h2>
-        <div className="card-actions min-w-max flex-nowrap items-center justify-between">
+        <h2 className="custom-text-ellipsis-1 card-title">Game Name adfasd fasdf asdf asdf asdfasdf</h2>
+        <div className="card-actions min-w-max items-center justify-between">
           {/* Price Badge */}
           <button className="badge badge-lg">£19.99</button>
           {/* But Now Button */}
