ee553fc04f0061d82a2dfecfa703bc2cc786a66e
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-01 11:50:14 +0000

    Updated Card styling and layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { MdOutlineAddShoppingCart } from 'react-icons/md';
 
 function Card() {
   return (
-    <div className="card-compact card bg-base-100 shadow-xl">
+    <div className="card-compact card bg-base-100 shadow-xl max-[430px]:card-side">
       <figure>
         {/* Card Image */}
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img className="h-full" src="https://placeimg.com/400/225/arch" alt="Shoes" />
       </figure>
-      <div className="card-body">
+      <div className="card-body relative">
         {/* Title */}
-        <h2 className="card-title">Game Name</h2>
-        {/* Description */}
-        <p>Game description goes here</p>
+        <h2 className="custom-text-ellipsis card-title overflow-hidden">Game Nameadfadf dfasdfasdfa sdasdfasdf</h2>
         <div className="card-actions items-center justify-between">
           {/* Price Badge */}
-          <button className="badge-secondary badge-outline badge badge-lg">£19.99</button>
+          <button className="badge badge-lg">£19.99</button>
           {/* But Now Button */}
-          <button className="btn-primary btn-sm btn">Buy Now</button>
+          <button className="btn-primary btn-sm btn">
+            <MdOutlineAddShoppingCart size={'1.3rem'} />
+          </button>
         </div>
       </div>
     </div>
