7a06b34b8ea9633e65fd5f730bde565d3ef183ad
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-01 09:19:22 +0000

    Updated homepage layout and styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex w-min flex-col gap-6 p-4">
+    <div className="relative -z-0 flex w-full items-center justify-center overflow-clip">
+      <div className="flex max-w-screen-2xl flex-col gap-4 p-4 sm:p-12">
         {/* Header */}
-        <h1 className="min-w-max text-7xl font-bold">
+        <h1 className="text-center text-[clamp(2rem,0.235rem+9.412vw,4rem)] font-bold leading-none sm:text-left">
           The Best Place To <br /> Find And Buy <br /> The Latest Games
         </h1>
 
         {/* Secondary Text */}
-        <p className="flex-wrap text-base">
+        <p className="flex-wrap text-center text-base sm:max-w-[60%] sm:text-left">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit beatae tempore excepturi quis non eligendi
           autem culpa
         </p>
 
         {/* Button */}
-        <button className="btn max-w-max">Explore Now</button>
-      </div>
+        <Link to="/store" className="btn-primary btn m-auto w-max sm:m-0">
+          Explore Now
+        </Link>
 
-      <div className="">
-        {/* Primary Image */}
-        <img src="https://placeimg.com/400/400/arch" alt="" />
+        {/* Styled circles */}
+        <div className="absolute -left-1/2 -top-[15%] -z-10 aspect-square w-[80%] rounded-full bg-secondary"></div>
+        <div className="absolute bottom-[5%] -right-[15%] -z-10 aspect-square w-[50%] rotate-45 rounded-full bg-accent"></div>
       </div>
     </div>
   );
