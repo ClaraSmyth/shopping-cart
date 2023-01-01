8e7f7d5c1f27672f3fab7b7a47eb3afb8be09854
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-01 04:05:09 +0000

    Updated checkout button in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar hidden h-min bg-base-100 transition-all sm:flex">
       {/* Logo */}
       <div>
         <Link to="/" className="btn-ghost btn text-xl normal-case">
-          Games{' '}
+          Games
         </Link>
       </div>
 
       {/* Nav Items */}
-      <ul className="menu rounded-box menu-horizontal flex-1 bg-base-100 p-2">
+      <ul className="menu rounded-box menu-horizontal flex-1 flex-nowrap bg-base-100 p-2">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -26,37 +26,26 @@ function Navbar() {
         <input type="text" placeholder="Search" className="input-bordered input" />
       </div>
 
-      {/* Checkout Dropdown */}
-      <div className="dropdown-end dropdown">
-        <label tabIndex={0} className="btn-ghost btn-circle btn">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <span className="badge badge-sm indicator-item">8</span>
-          </div>
-        </label>
-        <div tabIndex={0} className="card-compact card dropdown-content mt-6 w-52 bg-base-100 shadow">
-          <div className="card-body">
-            <span className="text-lg font-bold">8 Items</span>
-            <span className="text-info">Subtotal: $999</span>
-            <div className="card-actions">
-              <button className="btn-primary btn-block btn">View cart</button>
-            </div>
-          </div>
+      {/* Checkout Modal Button */}
+      <label tabIndex={0} htmlFor="checkout-modal" className="btn-ghost btn-circle btn">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+          <span className="badge badge-sm indicator-item">8</span>
         </div>
-      </div>
+      </label>
     </div>
   );
 }
