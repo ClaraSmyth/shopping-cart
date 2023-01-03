3c8854f3ec8e00dbbf375044c51b01334312b0f1
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-03 06:04:50 +0000

    Updated Card component interface and props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { MdOutlineAddShoppingCart } from 'react-icons/md';
+import { Game } from '../types';
 
 interface Props {
-  name: string;
-  price: number;
-  image: string;
+  key: number;
+  game: Game;
 }
 
 function Card(props: Props) {
-  const { name, price, image } = props;
+  const { game } = props;
   return (
     <div className="card-compact card overflow-hidden bg-base-100 shadow-xl max-[430px]:grid max-[430px]:grid-cols-[40%,1fr]">
       <figure className="h-28 overflow-hidden rounded-none">
         {/* Card Image */}
-        <img className="min-h-full object-cover" src={image} alt="Shoes" />
+        <img className="min-h-full object-cover" src={game.image} alt="Shoes" />
       </figure>
       <div className="card-body relative justify-between">
         {/* Title */}
-        <h2 className="custom-text-ellipsis-1 card-title">{name}</h2>
+        <h2 className="custom-text-ellipsis-1 card-title">{game.name}</h2>
         <div className="card-actions min-w-max items-center justify-between">
           {/* Price Badge */}
-          <button className="badge badge-lg">£{price}</button>
+          <button className="badge badge-lg">£{game.price}</button>
           {/* But Now Button */}
           <button className="btn-primary btn-sm btn">
             <MdOutlineAddShoppingCart size={'24px'} />
