35574ad409b5716af3ab0230ba10223fdec1bd23
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-03 06:19:07 +0000

    Updated Store, cleaned up code

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Card from './Card';
+import testData from '../testData.json';
 
 function Store() {
+  const data = testData.results.map((element) => ({ ...element, price: 19.99 }));
+
   return (
     <div className="grid auto-rows-max grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-4 overflow-scroll p-4">
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
-      <Card name={'Game Name'} price={19.99} image={'https://placeimg.com/400/400/arch'}></Card>
+      {data.map((obj) => (
+        <Card key={obj.id} game={obj} />
+      ))}
     </div>
   );
 }
