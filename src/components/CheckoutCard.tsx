eaa4c849ba4a4cd6866ac4a560e539eca8bcb214
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-03 09:31:11 +0000

    Select input now updates quantity in state

--- src/components/CheckoutCard.tsx
+++ src/components/CheckoutCard.tsx
@@ -5,10 +5,11 @@ import { Game } from '../types';
 interface Props {
   game: Game;
   removeFromCart: Function;
+  updateCartQty: Function;
 }
 
 function CheckoutCard(props: Props) {
-  const { game, removeFromCart } = props;
+  const { game, removeFromCart, updateCartQty } = props;
 
   return (
     <div className="card rounded-box h-16 flex-row bg-base-300 p-1">
@@ -17,7 +18,11 @@ function CheckoutCard(props: Props) {
       <div className="flex flex-1 flex-col overflow-auto pl-2">
         <p className="custom-text-ellipsis-1">{game.name}</p>
         <div className="mt-1 flex max-w-max gap-2">
-          <select className=" select-bordered select select-xs w-full max-w-max bg-[calc(100%-12px)calc(1px+50%),calc(100%-8px)calc(1px+50%)] pr-4 pl-1 text-center">
+          <select
+            onChange={(e) => updateCartQty(game, e.target.value)}
+            defaultValue={game.quantity}
+            className="select-bordered select select-xs w-full max-w-max bg-[calc(100%-12px)calc(1px+50%),calc(100%-8px)calc(1px+50%)] pr-4 pl-1 text-center"
+          >
             <option disabled>Qty</option>
             <option>1</option>
             <option>2</option>
