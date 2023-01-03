8451b20cf960ba5c1e4cd3b928208891c2c31447
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-03 09:29:02 +0000

    Added function to update item quantity in state

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   const addToCart = (obj: Game) => setCart((prev) => [...prev, obj]);
   const removeFromCart = (obj: Game) => setCart((prev) => [...prev.filter((item) => item.id !== obj.id)]);
+  const updateCartQty = (obj: Game, num: number) => {
+    setCart((prev) => [...prev.map((item) => (item.id === obj.id ? { ...item, quantity: num } : item))]);
+  };
 
   return (
     <div className="grid h-full overflow-x-hidden overflow-y-scroll bg-base-300 pb-16 sm:grid-rows-[min-content,1fr] sm:pb-0">
@@ -19,7 +22,7 @@ function App() {
       </Routes>
 
       <MobileNav />
-      <CheckoutModal cart={cart} removeFromCart={removeFromCart} />
+      <CheckoutModal cart={cart} removeFromCart={removeFromCart} updateCartQty={updateCartQty} />
     </div>
   );
 }
