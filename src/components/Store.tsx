941a3dd567f8a2cc78e3e6e72a383251a4cffa70
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-03 07:10:45 +0000

    Updated Store with props

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Card from './Card';
 import testData from '../testData.json';
 
-function Store() {
+interface Props {
+  addToCart: Function;
+}
+
+function Store(props: Props) {
+  const { addToCart } = props;
   const data = testData.results.map((element) => ({ ...element, price: 19.99 }));
 
   return (
     <div className="grid auto-rows-max grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-4 overflow-scroll p-4">
       {data.map((obj) => (
-        <Card key={obj.id} game={obj} />
+        <Card key={obj.id} game={obj} addToCart={addToCart} />
       ))}
     </div>
   );
