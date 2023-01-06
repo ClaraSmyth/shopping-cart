0d34d60e5c72f34ccea43abf7842533b3a5b9152
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-06 14:15:20 +0000

    Updated API call with search params

--- src/api/getGameList.ts
+++ src/api/getGameList.ts
@@ -4,10 +4,12 @@ import { IGameList } from '../types';
 
 const API_KEY = '2565b73cb1f84944a53974c646e776fa';
 
-async function getGameList(): Promise<IGameList> {
-  const response = await fetch(
-    `https://api.rawg.io/api/games?key=${API_KEY}&metacritic=80,100&dates=2020-01-01,2023-01-01`
-  );
+async function getGameList(search: string): Promise<IGameList> {
+  const date = new Date().toISOString().slice(0, 10);
+  const defaultParams = `&metacritic=80,100&dates=2020-01-01,${date}`;
+  const searchParams = `&ordering=metacritic=0,100&search=${search}`;
+
+  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}${search ? searchParams : defaultParams}`);
 
   if (response.status === 200) {
     const data = response.json();
