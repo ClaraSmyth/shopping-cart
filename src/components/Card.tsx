dd4c56d7f590a44868bfc6eab59f3cb9d06c1b70
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-06 03:25:39 +0000

    Cards onClick now navigate to correct route

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MdOutlineAddShoppingCart, MdOutlineRemoveShoppingCart } from 'react-icons/md';
 import { IUseCartOutput } from '../hooks/useCart';
 import { IGame } from '../types';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   game: IGame;
@@ -12,9 +13,13 @@ interface Props {
 function Card(props: Props) {
   const { game, cart, updateCart } = props;
   const isInCart = cart.some((item) => item.id === game.id);
+  const navigate = useNavigate();
 
   return (
-    <div className="card-compact card cursor-pointer overflow-hidden bg-base-100 shadow-xl transition hover:scale-105 max-[430px]:grid max-[430px]:grid-cols-[40%,1fr]">
+    <div
+      onClick={() => navigate(`/store/${game.slug}`)}
+      className="card card-compact cursor-pointer overflow-hidden bg-base-100 shadow-xl transition hover:scale-105 max-[430px]:grid max-[430px]:grid-cols-[40%,1fr]"
+    >
       <figure className="h-28 overflow-hidden rounded-none">
         {/* Card Image */}
         <img className="min-h-full object-cover" src={game.background_image} alt="Shoes" />
@@ -27,7 +32,10 @@ function Card(props: Props) {
           <button className="badge badge-lg">£{game.price}</button>
           {/* But Now Button */}
           <button
-            onClick={() => (isInCart ? updateCart.remove(game) : updateCart.add(game))}
+            onClick={(e) => {
+              e.stopPropagation();
+              isInCart ? updateCart.remove(game) : updateCart.add(game);
+            }}
             className={`btn-primary btn-sm btn ${isInCart && 'btn-error'}`}
           >
             {isInCart ? <MdOutlineRemoveShoppingCart size={'1.5em'} /> : <MdOutlineAddShoppingCart size={'1.5em'} />}
