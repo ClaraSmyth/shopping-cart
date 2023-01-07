153b926d4b81e1fc3e5c4154bffb3fde05548505
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-07 10:01:06 +0000

    Added a cache for previous API calls

--- src/api/getGameData.ts
+++ src/api/getGameData.ts
@@ -4,11 +4,17 @@ import { IGame } from '../types';
 
 const API_KEY = '2565b73cb1f84944a53974c646e776fa';
 
-async function getGameData(slug: string | undefined): Promise<IGame> {
+async function getGameData(slug: string): Promise<IGame> {
+  const cachedRequests = JSON.parse(sessionStorage.getItem('gameDataCache') || '{}');
+  if (cachedRequests[slug]) return cachedRequests[slug];
+
   const response = await fetch(`https://api.rawg.io/api/games/${slug}?key=${API_KEY}`);
 
   if (response.status === 200) {
-    const data = response.json();
+    const data = await response.json();
+    cachedRequests[slug] = data;
+    sessionStorage.setItem('gameDataCache', JSON.stringify(cachedRequests));
+
     return data;
   }
 
--- src/api/getGameList.ts
+++ src/api/getGameList.ts
@@ -7,12 +7,18 @@ const API_KEY = '2565b73cb1f84944a53974c646e776fa';
 async function getGameList(search: string): Promise<IGameList> {
   const date = new Date().toISOString().slice(0, 10);
   const defaultParams = `&metacritic=80,100&dates=2020-01-01,${date}`;
-  const searchParams = `&ordering=metacritic=0,100&search=${search}`;
+  const searchParams = `&ordering=metacritic=0,100&search=${search.trim()}`;
+  const params = search ? searchParams : defaultParams;
 
-  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}${search ? searchParams : defaultParams}`);
+  const cachedRequests = JSON.parse(sessionStorage.getItem('gameListCache') || '{}');
+  if (cachedRequests[params]) return cachedRequests[params];
+
+  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}${params}`);
 
   if (response.status === 200) {
-    const data = response.json();
+    const data = await response.json();
+    cachedRequests[params] = data;
+    sessionStorage.setItem('gameListCache', JSON.stringify(cachedRequests));
     return data;
   }
 
