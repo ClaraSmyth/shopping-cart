9e8005bbbf5ca05a531a71080f2bd1db9e8493b7
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-07 09:59:31 +0000

    Updated App with search state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navbar, Home, Store, CheckoutModal, MobileNav, Game } from './components';
 import { useCart } from './hooks';
 
 function App() {
   const { cart, updateCart } = useCart();
+  const [search, setSearch] = useState('');
 
   return (
     <div className="grid h-full overflow-x-hidden overflow-y-scroll bg-base-300 pb-16 sm:grid-rows-[min-content,1fr] sm:pb-0">
-      <Navbar cart={cart} />
+      <Navbar cart={cart} setSearch={setSearch} />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store cart={cart} updateCart={updateCart} />} />
+        <Route path="/store" element={<Store cart={cart} updateCart={updateCart} search={search} />} />
         <Route path="/store/:slug" element={<Game cart={cart} updateCart={updateCart} />} />
       </Routes>
 
