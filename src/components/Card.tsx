a2b4bb8e0c125a7f8b34b73a5411c8130db483cf
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-07 16:19:51 +0000

    Updated styling for larger screens

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,23 +20,23 @@ function Card(props: Props) {
       onClick={() => navigate(`/store/${game.slug}`)}
       className="card card-compact cursor-pointer overflow-hidden bg-base-100 shadow-xl transition hover:scale-105 max-[430px]:grid max-[430px]:grid-cols-[40%,1fr]"
     >
-      <figure className="h-28 overflow-hidden rounded-none">
+      <figure className="h-28 overflow-hidden rounded-none md:h-44">
         {/* Card Image */}
         <img className="min-h-full object-cover" src={game.background_image} alt="Shoes" />
       </figure>
-      <div className="card-body relative justify-between">
+      <div className="card-body relative justify-between bg-transparent">
         {/* Title */}
         <h2 className="custom-text-ellipsis-1 card-title">{game.name}</h2>
         <div className="card-actions min-w-max items-center justify-between">
           {/* Price Badge */}
-          <button className="badge badge-lg">£{game.price}</button>
+          <button className="badge badge-lg md:p-4 md:text-lg md:font-semibold">£{game.price}</button>
           {/* But Now Button */}
           <button
             onClick={(e) => {
               e.stopPropagation();
               isInCart ? updateCart.remove(game) : updateCart.add(game);
             }}
-            className={`btn-primary btn-sm btn ${isInCart && 'btn-error'}`}
+            className={`btn btn-primary btn-sm gap-2 md:h-9 md:min-h-[2.25rem] ${isInCart && 'btn-error'}`}
           >
             {isInCart ? <MdOutlineRemoveShoppingCart size={'1.5em'} /> : <MdOutlineAddShoppingCart size={'1.5em'} />}
           </button>
