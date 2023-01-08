351516879dac95a598e3ca9dfd99fe711a03ae6b
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-08 20:19:09 +0000

    Added animations to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SiGithub } from 'react-icons/si';
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
     <div className="relative -z-0 flex w-full items-center justify-center overflow-clip">
       <div className="flex max-w-screen-2xl flex-col place-items-center gap-4 p-4 sm:p-12">
         {/* Header */}
-        <h1 className="text-center text-[clamp(2rem,0.397rem+8.547vw,4.5rem)] font-bold leading-none">
-          The Best Place To <br /> Find And Buy <br /> The Latest Games
+        <h1 className="text-center text-[clamp(2rem,1.342rem+3.509vw,4.5rem)] font-bold leading-none">
+          The Best Place To <br /> Find And Buy <br /> The{' '}
+          <span
+            style={{
+              background: 'linear-gradient(to right, hsl(var(--s)), hsl(var(--a)))',
+              WebkitTextFillColor: 'transparent',
+              backgroundClip: 'text',
+            }}
+          >
+            Latest Games
+          </span>
         </h1>
 
         {/* Secondary Text */}
-        <p className="flex-wrap text-center text-[clamp(1rem,0.872rem+0.684vw,1.2rem)] text-base sm:max-w-[60%]">
+        <p className="flex-wrap text-center text-[clamp(0.9rem,0.821rem+0.421vw,1.2rem)] text-base sm:max-w-[60%]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit beatae tempore excepturi quis non eligendi
           autem culpa
         </p>
@@ -23,11 +33,19 @@ function Home() {
         </Link>
 
         {/* Styled circles */}
-        <div
+        <motion.div
+          initial={{ translateX: '-10%', translateY: '-10%', rotate: '10deg' }}
+          animate={{ translateX: '0', translateY: '0', rotate: '15deg' }}
+          transition={{ type: 'spring', stiffness: 30 }}
           style={{ backfaceVisibility: 'hidden', maskRepeat: 'no-repeat' }}
-          className="mask mask-star absolute -left-[9%] -top-[14vw] -z-10 aspect-square h-[50vw] rotate-12 bg-secondary-focus"
-        ></div>
-        <div className="absolute bottom-[5%] -right-[15%] -z-10 aspect-square w-[40%] rotate-45 rounded-full bg-accent"></div>
+          className="mask mask-star absolute -left-[9%] -top-[14vw] -z-10 aspect-square h-[50vw] rotate-45 bg-secondary"
+        ></motion.div>
+        <motion.div
+          initial={{ translateX: '10%', translateY: '10%' }}
+          animate={{ translateX: '0', translateY: '0' }}
+          transition={{ type: 'spring', stiffness: 30 }}
+          className="absolute bottom-[5%] -right-[15%] -z-10 aspect-square w-[40%] rounded-full bg-accent"
+        ></motion.div>
         <div className="absolute bottom-0 left-1 flex gap-2 text-sm text-base-content/40">
           <a className="flex items-center gap-1 font-semibold" href="https://github.com/ClaraSmyth">
             Clara Smyth <SiGithub />
